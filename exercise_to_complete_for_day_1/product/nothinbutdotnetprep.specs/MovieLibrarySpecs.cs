eb90ce413a75f360e98a72375337342a9d0736e7
Jean-Paul S. Boodhoo <jp@jpboodhoo.com>
2010-05-24 17:13:35 -0600

    Fixed the compile error

--- exercise_to_complete_for_day_1/product/nothinbutdotnetprep.specs/MovieLibrarySpecs.cs
+++ exercise_to_complete_for_day_1/product/nothinbutdotnetprep.specs/MovieLibrarySpecs.cs
@@ -188,7 +188,7 @@ namespace nothinbutdotnetprep.specs
   
         It should_be_able_to_find_all_movies_published_by_pixar = () =>
         {
-            var criteria = Where<Movie>.has_a(x => x.production_studio).equal_to(ProductionStudio.Pixar);
+            var criteria = Where.has_a(x => x.production_studio).equal_to(ProductionStudio.Pixar);
             var results = sut.all_movies().all_items_matching(criteria);
 
             results.ShouldContainOnly(cars, a_bugs_life);
