5f1892534ff00459378edbaf13594c85bc39018d
Jean-Paul S. Boodhoo <jp@jpboodhoo.com>
2010-05-24 14:48:30 -0600

    First mini exercise

--- exercise_to_complete_for_day_1/product/nothinbutdotnetprep/utility/extensions/EnumerableExtensions.cs
+++ exercise_to_complete_for_day_1/product/nothinbutdotnetprep/utility/extensions/EnumerableExtensions.cs
@@ -1,9 +1,15 @@
+using System;
 using System.Collections.Generic;
 
 namespace nothinbutdotnetprep.utility.extensions
 {
     public static class EnumerableExtensions
     {
+        public static IEnumerable<T> all_items_matching<T>(this IEnumerable<T> items, Predicate<T> criteria)
+        {
+            foreach (var item in items) if (criteria(item)) yield return item;
+        }
+
         public static IEnumerable<T> one_at_a_time<T>(this IEnumerable<T> items)
         {
             foreach (var item in items) yield return item;
