87bbd383457a9b69ac556e174c8d593451a8bf8e
Magesh <tkmagesh77@gmail.com>
2010-05-25 07:48:33 +0530

    Completing the exercise

--- exercise_to_complete_for_day_1/product/nothinbutdotnetprep/utility/searching/Where.cs
+++ exercise_to_complete_for_day_1/product/nothinbutdotnetprep/utility/searching/Where.cs
@@ -4,10 +4,48 @@ namespace nothinbutdotnetprep.utility.searching
 
     public class Where<ItemToFilter>
     {
-        public static PropertyAccessor<ItemToFilter,PropertyType> has_a<PropertyType>(
+
+       public static CriteriaFactory<ItemToFilter,PropertyType> has_a<PropertyType>(
             PropertyAccessor<ItemToFilter,PropertyType> accessor)
         {
-            return accessor;
+          
+            return new CriteriaFactory<ItemToFilter, PropertyType>(accessor);
         }
+
+        
+      }
+
+    public class CriteriaFactory<ItemToFilter, PropertyType>
+    {
+        public PropertyAccessor<ItemToFilter, PropertyType> accessor;
+
+        public CriteriaFactory(PropertyAccessor<ItemToFilter, PropertyType> accessor)
+        {
+            this.accessor = accessor;
+        }
+
+        public PredicateCriteria<ItemToFilter> equal_to(PropertyType propertyValue)
+        {
+            return new PredicateCriteria<ItemToFilter>(x => accessor(x).Equals(propertyValue));
+        }
+
+        public PredicateCriteria<ItemToFilter> not_equal_to(PropertyType propertyValue)
+        {
+            return new PredicateCriteria<ItemToFilter>(x => !accessor(x).Equals(propertyValue));
+        }
+        /*
+        public PredicateCriteria<ItemToFilter> equal_to_any(PropertyType[] propertyValues)
+        {
+            return new PredicateCriteria<ItemToFilter>(x =>
+            {
+                foreach (var propertyValue in propertyValues)
+                {
+                    if (accessor(x).Equals(propertyValue))
+                        break;
+                }
+                
+            });
+        }
+         * */
     }
 }
