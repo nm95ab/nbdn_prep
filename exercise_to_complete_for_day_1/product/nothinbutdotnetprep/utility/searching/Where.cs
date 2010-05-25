45a099b298632cc115745d0eb391252b15fe4fb9
Jean-Paul S. Boodhoo <jp@jpboodhoo.com>
2010-05-24 19:50:46 -0600

    Updated the Where class

--- exercise_to_complete_for_day_1/product/nothinbutdotnetprep/utility/searching/Where.cs
+++ exercise_to_complete_for_day_1/product/nothinbutdotnetprep/utility/searching/Where.cs
@@ -1,13 +1,30 @@
+using System;
+
 namespace nothinbutdotnetprep.utility.searching
 {
-    public delegate PropertyType PropertyAccessor<ItemToFilter,PropertyType>(ItemToFilter item);
+    public delegate PropertyType PropertyAccessor<ItemToFilter, PropertyType>(ItemToFilter item);
 
     public class Where<ItemToFilter>
     {
-        public static PropertyAccessor<ItemToFilter,PropertyType> has_a<PropertyType>(
-            PropertyAccessor<ItemToFilter,PropertyType> accessor)
+        public static CriteriaFactory<ItemToFilter, PropertyType> has_a<PropertyType>(
+            Func<ItemToFilter, PropertyType> accessor)
         {
-            return accessor;
+            return new CriteriaFactory<ItemToFilter, PropertyType>(accessor);
+        }
+    }
+
+    public class CriteriaFactory<ItemToFilter, PropertyType>
+    {
+        Func<ItemToFilter,PropertyType> accessor;
+
+        public CriteriaFactory(Func<ItemToFilter, PropertyType> accessor)
+        {
+            this.accessor = accessor;
+        }
+
+        public Criteria<ItemToFilter> equal_to(PropertyType value)
+        {
+            return new PredicateCriteria<ItemToFilter>(x => accessor(x).Equals(value));
         }
     }
 }
